fc64e7a2f2c75eb34d74f9b25055e2d4ec76b542
piyush rathore <piysuh260305@gmail.com>
2024-05-14 23:47:29 +0530

    made some changes

--- async/bg-color-changing/script.js
+++ async/bg-color-changing/script.js
@@ -1,12 +1,20 @@
-const hex="0123456789ABCDEF"
-let color="#";
-for(let i=0;i<6;i++){
-    color+=hex[Math.floor(Math.random()*16)]
-}
-const start=document.getElementById("btn-start")
-const end=document.getElementById("btn-end")
-start.addEventListener("click",()=>{
-    setInterval(()=>{
-        document.querySelector("body").style.backgroundColor=color
-    },1000)
-})
+const start = document.getElementById("btn-start");
+const end = document.getElementById("btn-end");
+const hex = "0123456789ABCDEF";
+let color = "#";
+let Interval;
+start.addEventListener("click", () => {
+  
+  Interval=setInterval(
+    (color) => {for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+      document.querySelector("body").style.backgroundColor = color;
+    },
+    1500,
+    color
+  );
+});
+end.addEventListener("click", () => {
+  clearInterval(Interval)
+});
