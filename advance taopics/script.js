2e0e73b2297896df791e3da5b510ddcbc28e6253
piyush rathore <piysuh260305@gmail.com>
2024-05-15 23:08:18 +0530

    getting image as well from the api

--- advance taopics/script.js
+++ advance taopics/script.js
@@ -1,4 +1,5 @@
-const requestUrl = "https://api.github.com/users/hiteshchoudhary";
+const profilePhoto=document.querySelector('.card')
+const requestUrl = "https://api.github.com/users/Piyushrathoree;
       const xhr = new XMLHttpRequest();//creating a xml/api request 
       xhr.open("GET", requestUrl);//this method shows what you have to perform and from where 
       xhr.onreadystatechange = function () {
@@ -7,7 +8,12 @@ const requestUrl = "https://api.github.com/users/hiteshchoudhary";
           const data = JSON.parse(this.responseText);//converting the data into object form because everything which is recieved from request is come in the form of string
           console.log(typeof data);
           console.log(data.followers);
-          console.log(data.location)
+          console.log(data.location) ;
+          const avatarUrl = data.avatar_url; // assuming the avatar URL is in the 'avatar_url' property
+          const img = document.createElement('img');
+          img.src = avatarUrl;
+          profilePhoto.appendChild(img);
+
 
         }
       };
