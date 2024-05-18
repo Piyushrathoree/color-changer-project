6ad70c6b667acdd3236b557f6815467be95a1316
piyush rathore <piysuh260305@gmail.com>
2024-05-18 07:11:19 +0530

    one more way or more optimized way to execute a promise using async await

--- promises/script2.js
+++ promises/script2.js
@@ -3,9 +3,35 @@ async function promise() {
   const response = new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("running promise");
+      resolve({'username':"js","password":'123'})
     }, 1000);
   });
   await response;
   console.log(response);
 }
 promise();
+//comment the above code whenever you want to run the below code
+
+
+
+//one more way to do the same thing
+const promiseId=new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    console.log("running promise");
+    resolve({'username':"js","password":'123'})
+    
+  },1000)
+  
+})
+
+async function consumePromise(){
+  // we want to handle error then we use try and catch here
+  try{
+    const response=await promiseId
+  console.log(response)
+
+  }catch{
+    console.log("error: something went wrong")
+  }
+}
+consumePromise()
